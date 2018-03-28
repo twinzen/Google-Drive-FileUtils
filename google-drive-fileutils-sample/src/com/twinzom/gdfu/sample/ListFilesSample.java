4e94dd954a291fdfa1cb16b4b9c1e37cb7a67a10
Twinsen Lai <twinzen@gmail.com>
2018-03-28 18:56:13 +0800

    ListFilesSample

--- google-drive-fileutils-sample/src/com/twinzom/gdfu/sample/ListFilesSample.java
+++ google-drive-fileutils-sample/src/com/twinzom/gdfu/sample/ListFilesSample.java
@@ -4,13 +4,18 @@ import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.services.drive.model.File;
 import com.twinzom.gdfu.Authorization;
 import com.twinzom.gdfu.GDFileUtils;
 
+/**
+ * @author twinzom
+ * 
+ * This class demonstrated how to use GDFU to get Google Drive files
+ *
+ */
 public class ListFilesSample {
 
 	static String KEY_FILE = "/com/twinzom/gdfu/sample/private-key.json";
@@ -18,12 +23,20 @@ public class ListFilesSample {
 	public static void main(String[] args) throws IOException, GeneralSecurityException {
 		GoogleCredential credential = Authorization.getCredential(KEY_FILE, Collections.singleton("https://www.googleapis.com/auth/drive"));
 		GDFileUtils gdfu = new GDFileUtils(credential);
-		Collection<File> files = gdfu.listFiles("", null);
-		for (File file : files) {
+		
+		Collection<File> allFiles = gdfu.listFiles("", null);
+		System.out.println("------------------------------------------------------------");
+		System.out.println("All files:");
+		for (File file : allFiles) {
 			System.out.println(file);
 		}
 		
-
+		Collection<File> files = gdfu.listFiles("0B64VHJrvrPWHc09wdHh0aXFOYjg", null);
+		System.out.println("------------------------------------------------------------");
+		System.out.println("Files under a folder (id:0B64VHJrvrPWHc09wdHh0aXFOYjg):");
+		for (File file : files) {
+			System.out.println(file);
+		}
 	}
 
 }
